e5fbf3b867bbfa282d72e982d92896c689d3d3fc
Nguyen Tuan Anh <anhnt796@gmail.com>
2018-07-16 16:57:07 +0700

    Chapter 13: User microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   scope :user_info, ->{select :id, :name, :email}
@@ -67,6 +68,10 @@ class User < ApplicationRecord
 
   def password_reset_expired?
     reset_sent_at < Settings.password.expired.time.HOUR.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
